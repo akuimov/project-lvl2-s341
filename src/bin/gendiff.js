5f9f1754867b603f1a8199bab42e1a9215131b1f
Kuimov Andrey <a_kuimov@inbox.ru>
2018-10-08 14:49:27 +0700

    Task 1

---
+++ src/bin/gendiff.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+// import { version } from '../../package.json';
+
+const program = require('commander');
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .option('-f, --format [type]', 'Output format');
+
+program.parse(process.argv);
